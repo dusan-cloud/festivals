1d3e076ec9407c36a202f5f7351b751a9458af46
dusan94 <d.ilisic@yahoo.com>
2021-07-16 11:24:17 +0200

    props reserve

--- front/src/components/tasks/Reserve.js
+++ front/src/components/tasks/Reserve.js
@@ -1,6 +1,26 @@
+import { useEffect, useState } from "react";
+import AppAxios from "../../apis/AppAxios";
 
- const Reserve = (props) => {
+const Reserve = (props) => {
+  const [festival, setFestival] = useState({});
 
- }
+  useEffect(() => {
+    getFestivalById(props.match.params.id);
+  }, []);
 
- export default Reserve;
+  const getFestivalById = async (id) => {
+    try {
+      const res = await AppAxios.get("/festivali/" + id);
+      console.log(res);
+      setFestival(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  
+
+
+};
+
+export default Reserve;
