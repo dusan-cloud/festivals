b1b5261edf2eb0f1e1d9a51819f26879130398be
dusan94 <d.ilisic@yahoo.com>
2021-07-17 12:10:07 +0200

    adding form

--- front/src/components/tasks/Reserve.js
+++ front/src/components/tasks/Reserve.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
+import { Form, Button } from "react-bootstrap";
 import AppAxios from "../../apis/AppAxios";
 
 const Reserve = (props) => {
   const [festival, setFestival] = useState({});
+  const [brojKarata, setBrojKarata] = useState(0);
 
   useEffect(() => {
     getFestivalById(props.match.params.id);
@@ -18,9 +20,28 @@ const Reserve = (props) => {
     }
   };
 
-  
+  function rezervisi () {
 
+  }
 
+  return (
+    <div>
+      <Form>
+        <Form.Group>
+          <Form.Label>Broj karata</Form.Label>
+          <Form.Control
+            type="number"
+            min="0"
+            name="brojKarata"
+            placeholder="Broj karata"
+            onChange={(e) => setBrojKarata(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Button onClick={() => rezervisi()}>Rezervisi</Button>
+      </Form>
+    </div>
+  );
 };
 
 export default Reserve;
